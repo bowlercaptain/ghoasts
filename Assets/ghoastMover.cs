4e0d3a8ec2c6a2310088bc028316ef048dca1bb3
Robert Mock <robertjamesmock@gmail.com>
2017-07-29 15:22:44 -0700

    init commit`

---
+++ Assets/ghoastMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghoastMover : MonoBehaviour {
+
+	Rigidbody myrig;
+
+	float mass;
+	public float speed;
+
+	private void Awake()
+	{
+		myrig = GetComponent<Rigidbody>();
+		mass = myrig.mass;
+		}
+
+	// Update is called once per frame
+	void Update () {
+		myrig.AddForce(new Vector3(Input.GetAxis("Horizontal")*mass * speed, Input.GetAxis("Vertical")*mass * speed));
+	}
+}
